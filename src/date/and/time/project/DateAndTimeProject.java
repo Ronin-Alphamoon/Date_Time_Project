62acb6865fd9f95f0e2e73d9fc021a62e32e6139
Ronin-Alphamoon <23alphamoon23gmail.com>
2022-08-22 11:16:49 +0100

    Initializing project

---
+++ src/date/and/time/project/DateAndTimeProject.java
@@ -0,0 +1,10 @@
+package date.and.time.project;
+
+public class DateAndTimeProject {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
