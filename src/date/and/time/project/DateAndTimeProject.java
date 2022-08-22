a22e8271207948773cdd226631b60b35fc19f3c5
Ronin-Alphamoon <23alphamoon23gmail.com>
2022-08-22 11:59:32 +0100

    Set up date class instance

--- src/date/and/time/project/DateAndTimeProject.java
+++ src/date/and/time/project/DateAndTimeProject.java
@@ -1,9 +1,21 @@
 package date.and.time.project;
 
+import java.util.Calendar;
+
 public class DateAndTimeProject {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        int month = 12,
+            year = 2021,
+            day = 23;
+       // Date userDate = new Date(year,month,day);
+        Calendar userDate = Calendar.getInstance();
+       
+        userDate.set(year, month, day);
+        
+        System.out.println(userDate);
         
     }
     
