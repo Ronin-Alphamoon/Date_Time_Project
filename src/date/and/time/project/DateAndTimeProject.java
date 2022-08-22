25eb5871a49f029e98f3cdb90d67fec688fb2e38
Ronin-Alphamoon <23alphamoon23gmail.com>
2022-08-22 13:07:37 +0100

    Added the date formatter

--- src/date/and/time/project/DateAndTimeProject.java
+++ src/date/and/time/project/DateAndTimeProject.java
@@ -1,10 +1,22 @@
 package date.and.time.project;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateAndTimeProject {
 
     public static void main(String[] args) {
         // TODO code application logic here
         
+        System.out.println( formatDate( new Date()) );
     }
     
+    public static String formatDate( Date myDate){
+        String formatedDateString;
+        
+        SimpleDateFormat dateFormatter = new SimpleDateFormat("EEEE yyy/dd/mm");
+        formatedDateString = dateFormatter.format(myDate);
+        
+        return "Your full date is " + formatedDateString;
+    }
 }
